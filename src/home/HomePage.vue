a1957c01dca3ae1ca8b4ea16c1dd30fc83544acb
karna <karnagogoi7@gmail.com>
2022-07-28 11:06:10 +0530

    Replace users list to table

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -1,43 +1,60 @@
 <template>
-    <div>
-        <h1>Hi {{account.user.firstName}}!</h1>
-        <p>You're logged in!!</p>
-        <h3>Users from secure api end point:</h3>
-        <em v-if="users.loading">Loading users...</em>
-        <span v-if="users.error" class="text-danger">ERROR: {{users.error}}</span>
-        <ul v-if="users.items">
-            <li v-for="user in users.items" :key="user.id">
-                {{user.firstName + ' ' + user.lastName}}
-                <span v-if="user.deleting"><em> - Deleting...</em></span>
-                <span v-else-if="user.deleteError" class="text-danger"> - ERROR: {{user.deleteError}}</span>
-                <br/><span class="badge badge-secondary">{{user.role}}</span>
-                <span v-if="user.role == 'Auditor' "> - <a @click="deleteUser(user.id)" class="badge badge-danger">Delete</a></span>
-            </li>
-        </ul>
-        <p>
-            <router-link to="/login">Logout</router-link>
-        </p>
-    </div>
+  <div>
+    <h1>Hi {{ account.user.firstName }}!</h1>
+    <p>You're logged in!!</p>
+    <h3>Users from secure api end point:</h3>
+    <em v-if="users.loading">Loading users...</em>
+    <span v-if="users.error" class="text-danger">ERROR: {{ users.error }}</span>
+    <table class="table" v-if="users.items">
+      <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Role</th>
+          <th scope="col">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users.items" :key="user.id">
+          <td>
+            {{ user.firstName + " " + user.lastName }}
+          </td>
+          <td>
+            <span class="badge badge-secondary">{{ user.role }}</span>
+          </td>
+          <td>
+            <span v-if="user.role == 'Auditor'">
+              <a @click="deleteUser(user.id)" class="badge badge-danger"
+                >Delete</a
+              ></span
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <p>
+      <router-link to="/login">Logout</router-link>
+    </p>
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
-    computed: {
-        ...mapState({
-            account: state => state.account,
-            users: state => state.users.all
-        })
-    },
-    created () {
-        this.getAllUsers();
-    },
-    methods: {
-        ...mapActions('users', {
-            getAllUsers: 'getAll',
-            deleteUser: 'delete'
-        })
-    }
+  computed: {
+    ...mapState({
+      account: (state) => state.account,
+      users: (state) => state.users.all,
+    }),
+  },
+  created() {
+    this.getAllUsers();
+  },
+  methods: {
+    ...mapActions("users", {
+      getAllUsers: "getAll",
+      deleteUser: "delete",
+    }),
+  },
 };
 </script>
